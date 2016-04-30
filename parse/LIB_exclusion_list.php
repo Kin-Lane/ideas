2a680d599371d23c195193a7b549e39f08bc8e01
Kin Lane <info@apievangelist.com>
2016-04-29 18:03:55 -0700

    clean house

--- parse/LIB_exclusion_list.php
+++
@@ -1,5 +0,0 @@
-<?php
-# The spider will exclude any links containing any of the following substrings
-$exclusion_array[] = "googlesyndication";         // Exclude Google AdWords links.
-$exclusion_array[] = "doubleclick.net";           // Exclude doubleclick banner ads
-?>
